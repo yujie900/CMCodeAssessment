80f330a64de9cab3f833bc9caa07a1677105214e
yujie900 <ywu24@uh.edu>
2015-10-16 15:51:24 -0500

    initial commit

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public FizzBuzz ()
+        {
+            CustomTexts = new Dictionary<int, string>();
+        }
+        private Dictionary<int, string> CustomTexts;
+        public List<string> GetFizzBuzz(int lowerBound, int upperBound)
+        {
+            List<string> results = new List<string>();
+            for (int i = lowerBound; i<=upperBound;i++)
+            {
+                if (i % 3 == 0 && i %5 == 0)
+                {
+                    results.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    results.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    results.Add("Buzz");
+                }
+                else
+                {
+                    results.Add(i.ToString());
+                }
+            }
+            return results;
+        }
+        public void SetCustom(int index, string displayText)
+        {
+            CustomTexts.Add(index, displayText);
+        }
+        public List<string> GetCustom(int lowerBound, int upperBound)
+        {
+            List<string> results = new List<string>();
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                bool hasAdded = false;
+                foreach (int key in CustomTexts.Keys)
+                {
+                    if (i % key == 0)
+                    {
+                        if (!hasAdded)
+                        {
+                            results.Add(CustomTexts[key]);
+                            hasAdded = true;
+                        }
+                        else
+                        {
+                            results[i-1] = results[i-1] + CustomTexts[key];
+                        }
+                    }
+                }
+                if (!hasAdded)
+                {
+                    results.Add(i.ToString());
+                }
+            }
+            return results;
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class TestFizzBuzz
+    {
+        private FizzBuzz TestInstance;
+        private int LowerBound;
+        private int UpperBound;
+        [SetUp]
+        public void Init()
+        {
+            TestInstance = new FizzBuzz();
+
+            LowerBound = 1;
+            UpperBound = 20;
+        }
+        [Test]
+        public void TestGetFizzBuzz()
+        {
+            List<string> actualResults = TestInstance.GetFizzBuzz(LowerBound, UpperBound);
+
+            Assert.AreEqual("Fizz", actualResults[2]);
+            Assert.AreEqual("Buzz", actualResults[4]);
+            Assert.AreEqual("FizzBuzz", actualResults[14]);
+        }
+        [Test]
+        public void TestGetCustom()
+        {
+            TestInstance.SetCustom(2, "Clear");
+            TestInstance.SetCustom(5, "Measure");
+
+            List<string> actualResults = TestInstance.GetCustom(LowerBound, UpperBound);
+
+            Assert.AreEqual("Clear", actualResults[1]);
+            Assert.AreEqual("Measure", actualResults[4]);
+            Assert.AreEqual("Clear", actualResults[3]);
+            Assert.AreEqual("ClearMeasure", actualResults[9]);
+        }
+    }
+}
